bc36553251c61c45ae07d3b141a444adfdf07060
NilsGke <n.j.goeke@gmail.com>
2023-07-30 18:58:55 +0200

    spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,14 @@
+import { twMerge } from "tailwind-merge";
+
+export default function Spinner({ className }: { className: string }) {
+  return (
+    <div
+      className={twMerge(
+        "aspect-square flex justify-center items-center",
+        className
+      )}
+    >
+      <div className="animate-spin rounded-full h-full w-32 border-b-2 border-gray-900"></div>
+    </div>
+  );
+}
