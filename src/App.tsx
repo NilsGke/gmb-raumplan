adaa06a6a0b3e811480e2511826ed0446306032a
NilsGke <n.j.goeke@gmail.com>
2023-07-30 19:05:12 +0200

    load data, control buttons and search box

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,75 @@
-import { useRef } from "react";
+import { ReactElement, useEffect, useRef, useState } from "react";
 import Map from "@assets/map.svg";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import data, { loadElements } from "./data";
+import { Data, loadElements } from "./data";
+import SearchBox from "./components/SearchBox";
+import Spinner from "./components/Spinner";
+import { twMerge } from "tailwind-merge";
 
 function App() {
   const overlayElementRef = useRef<HTMLDivElement>(null);
   const objectElementRef = useRef<HTMLObjectElement>(null);
+  const [data, setData] = useState<Data | undefined>(undefined);
 
   const setup = async () => {
-    console.log("setup");
+    if (
+      objectElementRef.current === null ||
+      objectElementRef.current.contentDocument === null
+    )
+      return;
 
-    loadElements(objectElementRef).then(() => console.log("loaded", data));
+    loadElements(objectElementRef).then((_data) => setData(_data));
   };
+
+  const [loadData, setLoadData] = useState(true);
+  useEffect(() => {
+    if (
+      loadData &&
+      !!objectElementRef.current?.contentDocument?.querySelector("svg")
+    )
+      setup();
+    else {
+      setLoadData(false);
+      setTimeout(() => setLoadData(true), 40);
+    }
+  }, [loadData]);
+
   return (
     <>
       <div className="absolute w-screen h-screen">
-        <TransformWrapper centerOnInit centerZoomedOut={false}>
-          {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+        <Spinner
+          className={twMerge(
+            "absolute h-20 w-20 bottom-[calc(50%-5rem/2)] left-[calc(50%-5rem/2)] transition-all duration-500",
+            data !== undefined ? "opacity-0" : "opacity-100"
+          )}
+        />
+        <TransformWrapper centerOnInit centerZoomedOut={false} smooth={false}>
+          {({ zoomIn, zoomOut, resetTransform, zoomToElement }) => (
             <>
-              <div className="absolute bottom-0 left-0">
-                <button onClick={() => zoomIn()}>+</button>
-                <button onClick={() => zoomOut()}>-</button>
-                <button onClick={() => resetTransform()}>x</button>
+              {data === undefined ? (
+                <Spinner className="fixed h-10 w-10 bottom-5 right-5" />
+              ) : (
+                <SearchBox
+                  zoomToElement={(element: HTMLElement) =>
+                    zoomToElement(element, 3, 500, "easeInQuad")
+                  }
+                  overlayRef={overlayElementRef}
+                  data={data}
+                />
+              )}
+              <div className="fixed bottom-3 left-3 z-10 flex flex-col gap-2">
+                <ControlButton onClick={() => zoomIn()}>+</ControlButton>
+                <ControlButton onClick={() => zoomOut()}>-</ControlButton>
+                <ControlButton onClick={() => resetTransform()}>
+                  0
+                </ControlButton>
               </div>
               <TransformComponent
                 wrapperClass="w-full h-full"
                 contentClass="w-full h-full relative"
               >
                 <div
-                  className="absolute h-full w-full"
+                  className="absolute h-full w-full z-0 cursor-grab"
                   ref={overlayElementRef}
                 />
                 <object
@@ -36,15 +77,31 @@ function App() {
                   type="image/svg+xml"
                   className="w-full h-full"
                   ref={objectElementRef}
-                  onLoad={setup}
+                  onChange={setup}
                 ></object>
-                {/* <img src={Map} alt="" className="h-full " /> */}
               </TransformComponent>
             </>
           )}
         </TransformWrapper>
       </div>
     </>
+  );
+}
+
+function ControlButton({
+  children,
+  onClick,
+}: {
+  children: ReactElement | string;
+  onClick: () => void;
+}) {
+  return (
+    <button
+      className="rounded-full cursor-pointer aspect-square bg-zinc-800 h-10"
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
 
