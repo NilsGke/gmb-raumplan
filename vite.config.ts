c550f8baa58edb23d35cf34646fd51e94d30ffe6
NilsGke <n.j.goeke@gmail.com>
2023-08-26 00:46:57 +0200

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,47 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["icon.svg", "GMB-Logo.svg", "GMB-Logox512.jpg"],
+      manifest: {
+        name: "GMB - Raumplan",
+        short_name: "GMB - Raumplan",
+        description: "Der Raumplan des Gymnasiums am Mosbacher Berg",
+        icons: [
+          {
+            src: "icon192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "icon512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+
+        theme_color: "#035eab",
+        categories: ["navigation", "productivity"],
+        display: "minimal-ui",
+        display_override: ["minimal-ui", "browser"],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg}"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
